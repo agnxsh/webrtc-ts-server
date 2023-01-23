b2ae01e287db3f5c1219d33f4d1e6ef671a37021
Agnish Ghosh <iamagnix@gmail.com>
2023-01-23 13:06:22 +0530

    🏄: socket added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("it works!");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const port = 8080;
+const app = express();
+app.use(cors);
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user is connected successfully!");
+  socket.on("disconnect", () => {
+    console.log("user is disconnected!");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening to the server on ${port}`);
+});
